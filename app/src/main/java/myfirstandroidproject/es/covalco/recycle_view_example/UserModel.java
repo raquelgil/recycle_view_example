e4be0c28524b212ea798e0b17fed00c3eba4f7cb
Raquel Gil <rgil@covalco.es>
2019-06-03 12:19:29 +0200

    configuracio RecycleView

---
+++ app/src/main/java/myfirstandroidproject/es/covalco/recycle_view_example/UserModel.java
@@ -0,0 +1,12 @@
+package myfirstandroidproject.es.covalco.recycle_view_example;
+
+public class UserModel {
+
+    public UserModel() {
+    }
+
+    public UserModel(String name) {this.name = name; }
+    private String name;
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+}
